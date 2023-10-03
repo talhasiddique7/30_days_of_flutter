340f420f90bb0a46328ddbe5d28d203baacfc232
TALHA SIDDIQUE <ts9740743@gmail.com>
2023-10-03 15:55:24 +0500

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:days_30_flutter/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,17 +6,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+  //(DAY-2)DATA TYPES AND SCAFFOLD
+  //int days = 30;   //hold integers
+  //String name = 'PROFESSOR';   //hold string
+  //bool check = true; //hold true and false (0,1)
+  //double dec = 12.3; //hold decimal values
+  //var a = "hello";  //hold any 
+  //final s = 12;     //data modify
+  //const PI = 3.14;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("30 Days of Flutter"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
