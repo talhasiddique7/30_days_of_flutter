03e10af026bbc8a109098d86e406106792cfc484
talhasiddique7 <ts9740743@gmail.com>
2023-10-17 19:12:05 +0500

    day 13 done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,33 @@
+//Day 13 - Local Files | Load & Decode JSON
 import 'package:days_30_flutter/models/catalog.dart';
 import 'package:days_30_flutter/widgets/ItemWidgets.dart';
 import 'package:days_30_flutter/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final DummyList = List.generate(10, (index) => Products.items[0]);
+
+  get rootBundle => null;
+
+  void initState() {
+    super.initState();
+    LoadData();
+  }
+
+  LoadData() async {
+    final CatalogJson = await rootBundle.toString('images/files/catalog.json');
+    final decodejson = jsonDecode(CatalogJson);
+    final ProductsJson = decodejson['products'];
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -25,11 +45,9 @@ class HomePage extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(14.0),
-          child: ListView.builder(
-              
-              itemBuilder: ((context, index) {
-                return itemwidget(item: DummyList[index]);
-              })),
+          child: ListView.builder(itemBuilder: ((context, index) {
+            return itemwidget(item: DummyList[index]);
+          })),
         ),
         drawer: MyDrawer(),
       ),
